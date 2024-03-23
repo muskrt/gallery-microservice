4d79094b967de404cb4a01e0ade65915b2d9e6b1
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-23 18:16:25 +0300

    html files added for landing service

--- landing_service/main.go
+++ landing_service/main.go
@@ -1,5 +1,35 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func loginGetController(ctx *gin.Context) {
+
+	ctx.HTML(http.StatusOK, "login.html", nil)
+}
+
+func loginPostController(ctx *gin.Context) {
+
+	username := ctx.PostForm("username")
+	fmt.Println("username is -------" + username)
+	// sucess, _ := http.Get("login_service/")
+	// fmt.Println(sucess.Close)
+	ctx.HTML(http.StatusOK, "success.html", nil)
+}
 func main() {
+
+	server := gin.New()
+
+	server.LoadHTMLGlob("templates/*.html")
+	server.GET("/login", loginGetController)
+	server.POST("/login", loginPostController)
+
+	fmt.Println("landing service starting on 8080")
+
+	server.Run(":8080")
 
 }
