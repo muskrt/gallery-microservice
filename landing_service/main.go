657246f8fc6b8e78bcee9a01f6ed7977c029e57e
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-23 20:03:33 +0300

    landing service updated

--- landing_service/main.go
+++ landing_service/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,14 +14,33 @@ func loginGetController(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "login.html", nil)
 }
 
+type loginResponse struct {
+	Message string `json:"message"`
+}
+
 func loginPostController(ctx *gin.Context) {
 
 	username := ctx.PostForm("username")
+	password := ctx.PostForm("password")
 	fmt.Println("username is -------" + username)
-	//resp, _ := http.Get("login_service/"+username+"&"+password)
-	// fmt.Println(sucess.Close)
-	ctx.HTML(http.StatusOK, "success.html", nil)
+	resp, _ := http.Get("login_service/check/" + username + "/" + password)
+	res := &loginResponse{}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = json.Unmarshal(body, &res)
+	if err != nil {
+		fmt.Println(err)
+	}
+	if res.Message == "success" {
+		ctx.HTML(http.StatusOK, "success.html", nil)
+	}
+	ctx.HTML(http.StatusOK, "error.html", nil)
+
 }
+
 func main() {
 
 	server := gin.New()
