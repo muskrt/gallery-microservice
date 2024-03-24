9acdf262e668c557718cd9d9ab46de3fac5290ed
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-24 22:43:45 +0300

    test

--- Dev-build-packages.sh
+++ Dev-build-packages.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 echo "Running build script "
 echo $PWD
+cd landing_service 
+go mod download 
+go mod tidy
+cd ../login_service
+go mod download 
+go mod tidy
+cd ../
 go build -C ./landing_service
 go build -C ./login_service
