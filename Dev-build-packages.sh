67f328fce06b0dcf5dbf24549e02649e635290b0
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-24 22:29:06 +0300

    test

--- Dev-build-packages.sh
+++ Dev-build-packages.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 echo "Running build script "
 echo $PWD
-cd landing_service 
-export GOPROXY=direct
-
-go clean --modcache
-go get -u
-cd ../login_service 
-go get -u 
-cd ../
-
 go build -C ./landing_service
 go build -C ./login_service
