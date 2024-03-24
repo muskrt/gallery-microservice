6a02cf1ec4c4954e3db23815522d1c45727669b3
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-24 18:13:41 +0300

    coverage generate script added

---
+++ Dev-generate-coverage.sh
@@ -0,0 +1,8 @@
+services=`ls  | grep service`
+for service in $services 
+do 
+    cd $service 
+    go test -coverprofile=coverage.txt
+    gocover-cobertura < coverage.txt > coverage.xml
+    cd ../
+done 
