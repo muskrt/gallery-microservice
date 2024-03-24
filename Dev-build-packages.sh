59f671b7184d24f5bd6265a9d6346b99d840feaf
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-24 21:05:19 +0300

    ci job updated

--- Dev-build-packages.sh
+++ Dev-build-packages.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+echo "Running build script "
+echo $PWD
 cd landing_service && go mod download 
 cd ../login_service && go mod download && cd ../
+
 go build -C ./landing_service
 go build -C ./login_service
