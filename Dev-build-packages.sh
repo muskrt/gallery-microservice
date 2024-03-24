c22056f9ebbe0d37f13b349d5440ea506e6f09db
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-24 21:02:38 +0300

    scripts updated

--- Dev-build-packages.sh
+++ Dev-build-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd landing_service && go mod download 
 cd ../login_service && go mod download && cd ../
-go build -C landing_service
-go build -C login_service
+go build -C ./landing_service
+go build -C ./login_service
