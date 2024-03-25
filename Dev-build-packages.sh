b348d0450b74db84288a66c91e52ad2adf5b8d74
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-25 12:14:51 +0300

    test

--- Dev-build-packages.sh
+++ Dev-build-packages.sh
@@ -2,6 +2,6 @@
 echo "Running build script "
 echo $PWD
 cd landing_service 
-docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine go mod download && go get -u && go build .
+docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine 'go mod download && go get -u && go build . '
 cd ../login_service
-docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine go mod download && go get -u && go build .
+docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine 'go mod download && go get -u && go build . '
