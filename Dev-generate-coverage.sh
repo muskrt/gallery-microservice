ab78c481725937f6d0c7b9d9d529c0be716f4168
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-25 19:38:46 +0300

    test

--- Dev-generate-coverage.sh
+++ Dev-generate-coverage.sh
@@ -4,7 +4,7 @@ echo "running coverage script"
 for service in $services 
 do 
     cd $service 
-     docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine /bin/sh -c "go test -coverprofile=cover.txt  "
+    docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine /bin/sh -c "go test -coverprofile=cover.txt  "
     docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine /bin/sh -c "go tool cover -func=cover.txt"
     docker run --rm -v `pwd`:/app -w /app scottkurt/gallery:cobertura /bin/sh -c "gocover-cobertura < cover.txt > coverage.xml"
     cd ../
