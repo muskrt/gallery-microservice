f9c15d186fe7b62ea8d9b6e0616bbb770dc13313
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-25 12:42:18 +0300

    test

--- Dev-generate-coverage.sh
+++ Dev-generate-coverage.sh
@@ -3,8 +3,8 @@ services=`ls  | grep '_service'`
 for service in $services 
 do 
     cd $service 
-     docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine go tool cover -func=coverage.txt 
-    docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine go test -coverprofile=coverage.txt 
+     docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine go test -coverprofile=coverage.txt  
+    docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine go tool cover -func=coverage.txt
     docker run --rm -v `pwd`:/app -w /app keemyb/gocover-cobertura gocover-cobertura < coverage.txt > coverage.xml
     cd ../
 done 
