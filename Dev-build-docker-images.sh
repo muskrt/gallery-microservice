6744111a0341911d0da1917cdddbcc7da9703413
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-30 20:45:40 +0300

    test

--- Dev-build-docker-images.sh
+++ Dev-build-docker-images.sh
@@ -11,3 +11,12 @@ do
 done 
 docker build --force-rm -t "${repo}:landing_service_dev_${BUILD_NUMBER}" ./landing_service
 docker build --force-rm -t "${repo}:login_service_dev_${BUILD_NUMBER}" ./login_service
+images=`docker image ls | grep -i scottkurt/gallery | awk '{print $2}'`
+services=`docker image ls | awk '{print $1}' | grep service `
+echo "removing  images"
+for image in $images 
+do 
+    image=${repo}:${image}
+    echo $image
+    [  -z "$(docker images -q ${image})" ] || docker  image rm $image 
+done 
--- nightly/build-tag-docker-images.sh
+++ nightly/build-tag-docker-images.sh
@@ -1,11 +1,23 @@
+echo "Running Image Build Script"
+build_env=nightly
 repo=scottkurt/gallery
 images=`docker image ls | grep -i scottkurt/gallery | awk '{print $2}'`
 services=`docker image ls | awk '{print $1}' | grep service `
+echo "removing old images"
 for image in $images 
 do 
     image=${repo}:${image}
     echo $image
     [  -z "$(docker images -q ${image})" ] || docker  image rm $image 
 done 
-docker build --force-rm -t "landing_service:nightly_${BUILD_NUMBER}" ../landing_service
-docker build --force-rm -t "login_service:nightly_${BUILD_NUMBER}" ../login_service
+docker build --force-rm -t "${repo}:landing_service_${build_env}_${BUILD_NUMBER}" ${workspace}/landing_service
+docker build --force-rm -t "${repo}:login_service_${build_env}_${BUILD_NUMBER}" ${workspace}/login_service
+images=`docker image ls | grep -i scottkurt/gallery | awk '{print $2}'`
+services=`docker image ls | awk '{print $1}' | grep service `
+echo "removing  images"
+for image in $images 
+do 
+    image=${repo}:${image}
+    echo $image
+    [  -z "$(docker images -q ${image})" ] || docker  image rm $image 
+done 
