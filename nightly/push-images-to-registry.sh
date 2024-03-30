c21bf9a5898c95b3a22ece3b5f1d31bdac1ea3fb
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-30 21:09:09 +0300

    nightly scripts

---
+++ nightly/push-images-to-registry.sh
@@ -0,0 +1,18 @@
+repo=scottkurt/gallery
+images=`docker image ls | grep -i scottkurt/gallery | awk '{print $2}'| grep nightly`
+docker login docker.io -u ${DOCKER_USERNAME} -p ${DOCKER_PASSWORD}
+echo "pushing   images to registry"
+for image in $images 
+do 
+    image=${repo}:${image}
+    docker push $image
+done 
+images=`docker image ls | grep -i scottkurt/gallery | awk '{print $2}'`
+services=`docker image ls | awk '{print $1}' | grep service `
+echo "removing  images"
+for image in $images 
+do 
+    image=${repo}:${image}
+    echo $image
+    [  -z "$(docker images -q ${image})" ] || docker  image rm $image 
+done 
