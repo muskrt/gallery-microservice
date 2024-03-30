5e5de6ad2b0427b73cac1d7cf9a9e8c00abc802e
mustafa kurt <kurtmustafa5003@gmail.com>
2024-03-30 20:58:48 +0300

    test

--- Dev-generate-coverage.sh
+++ Dev-generate-coverage.sh
@@ -6,6 +6,7 @@ do
     cd $service 
     docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine /bin/sh -c "go test -coverprofile=cover.txt  "
     docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine /bin/sh -c "go tool cover -func=cover.txt"
-    docker run --rm -v `pwd`:/app -w /app scottkurt/gallery:cobertura /bin/sh -c "gocover-cobertura < cover.txt > coverage.xml"
+    gocover-cobertura < cover.txt > coverage.xml
+    #docker run --rm -v `pwd`:/app -w /app scottkurt/gallery:cobertura /bin/sh -c "gocover-cobertura < cover.txt > coverage.xml"
     cd ../
 done 
--- nightly/build-tag-docker-images.sh
+++ nightly/build-tag-docker-images.sh
@@ -10,14 +10,5 @@ do
     echo $image
     [  -z "$(docker images -q ${image})" ] || docker  image rm $image 
 done 
-docker build --force-rm -t "${repo}:landing_service_${build_env}_${BUILD_NUMBER}" ../landing_service
-docker build --force-rm -t "${repo}:login_service_${build_env}_${BUILD_NUMBER}" ../login_service
-images=`docker image ls | grep -i scottkurt/gallery | awk '{print $2}'`
-services=`docker image ls | awk '{print $1}' | grep service `
-echo "removing  images"
-for image in $images 
-do 
-    image=${repo}:${image}
-    echo $image
-    [  -z "$(docker images -q ${image})" ] || docker  image rm $image 
-done 
+docker build --force-rm -t "${repo}:landing_service_${build_env}_${BUILD_NUMBER}" ./landing_service
+docker build --force-rm -t "${repo}:login_service_${build_env}_${BUILD_NUMBER}" ./login_service
