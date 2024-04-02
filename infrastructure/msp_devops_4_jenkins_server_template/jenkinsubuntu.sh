1d759fcff19765d4ae6c84f303836a5851dacf45
mustafa kurt <kurtmustafa5003@gmail.com>
2024-04-02 23:40:13 +0300

    test

--- infrastructure/msp_devops_4_jenkins_server_template/jenkinsubuntu.sh
+++ infrastructure/msp_devops_4_jenkins_server_template/jenkinsubuntu.sh
@@ -38,6 +38,7 @@ curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compo
 chmod +x /usr/local/bin/docker-compose
 cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service.bak
 sed -i 's/^ExecStart=.*/ExecStart=\/usr\/bin\/dockerd -H tcp:\/\/127.0.0.1:2375 -H unix:\/\/\/var\/run\/docker.sock/g' /lib/systemd/system/docker.service
+systemctl daemon-reload
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 ./aws/install
