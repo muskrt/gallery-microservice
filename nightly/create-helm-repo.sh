0498f6ff4fcebfefd12f545b6880e9e51e689242
mustafa kurt <kurtmustafa5003@gmail.com>
2024-04-09 18:11:18 +0300

    test

--- nightly/create-helm-repo.sh
+++ nightly/create-helm-repo.sh
@@ -1,6 +1,7 @@
 export PATH=$PATH:/usr/local/bin
 AWS_REGION=us-east-1
 HELM_S3_MODE=3 
+aws s3 rm s3://gallery-helm-charts --recursive
 aws s3api create-bucket --bucket gallery-helm-charts --region us-east-1
 aws s3api put-object --bucket gallery-helm-charts --key stable/gallery/
 helm s3 init s3://gallery-helm-charts/stable/gallery 
