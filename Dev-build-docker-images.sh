54624f8fba81ba7e220c47ed09caef26d40b53b0
mustafa kurt <kurtmustafa5003@gmail.com>
2024-04-09 18:22:06 +0300

    test

--- Dev-build-docker-images.sh
+++ Dev-build-docker-images.sh
@@ -1,7 +1,7 @@
 echo "Running Image Build Script"
 repo=scottkurt/gallery
 images=`docker image ls | grep -i scottkurt/gallery | awk '{print $2}'`
-services=`ls  | grep '_service' | tr '_' '-'`
+services=`ls  | grep '_service' `
 echo "removing old images"
 for image in $images 
 do 
@@ -12,7 +12,7 @@ done
 for service in $services 
 do 
 echo "docker build --force-rm -t "${repo}:${service}_dev_${BUILD_NUMBER}" ./${service}"
-docker build --force-rm -t "${repo}:${service}_dev_${BUILD_NUMBER}" ./${service}
+docker build --force-rm -t "${repo}:`echo ${service}|tr '_' '-'`_dev_${BUILD_NUMBER}" ./${service}
 done
 
 
