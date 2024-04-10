11d417207d9a64ab353ad7b1848107b0f2dd18ed
mustafa kurt <kurtmustafa5003@gmail.com>
2024-04-10 15:15:42 +0300

    unit test script

---
+++ nightly/unit-tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+services=`ls  | grep '_service'`
+echo "running coverage script"
+for service in $services 
+do 
+    cd $service 
+    docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine /bin/sh -c "go test -coverprofile=cover.txt  "
+    docker run --rm -v `pwd`:/app -w /app golang:1.22-alpine /bin/sh -c "go tool cover -func=cover.txt"
+    docker run --rm -v `pwd`:/app -w /app scottkurt/gallery:cobertura /bin/sh -c "gocover-cobertura < cover.txt > coverage.xml"
+    cd ../
+done 
