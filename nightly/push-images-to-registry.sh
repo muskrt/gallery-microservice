756e7c98b156cfebba35b9161beb47999a4d14ec
mustafa kurt <kurtmustafa5003@gmail.com>
2024-04-10 17:09:49 +0300

    test

--- nightly/push-images-to-registry.sh
+++ nightly/push-images-to-registry.sh
@@ -7,3 +7,13 @@ do
     image=${repo}:${image}
     docker push $image
 done 
+build_env=nightly
+repo=scottkurt/gallery
+images=`docker image ls | grep -i scottkurt/gallery | grep -i nightly | awk '{print $2}'`
+echo "removing old images"
+for image in $images 
+do 
+    image=${repo}:${image}
+    echo $image
+    [  -z "$(docker images -q ${image})" ] || docker  image rm $image 
+done 
